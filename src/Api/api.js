5a977ece7cd678a2e2027937c0223580c4d7fff2
anushkasj06 <anushkasjadhav1@gmail.com>
2026-01-31 14:12:22 +0530

    bckend url added

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,20 +1,20 @@
+import axios from "axios";
 
-import axios from 'axios';
-const DEPLOYED='https://e-commerce-server-production-0873.up.railway.app'
-const LOCALHOST='http://localhost:5454'
-
-export const API_BASE_URL = LOCALHOST
-
-import.meta.env.VITE_API_BASE_URL || DEPLOYED;
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:5454";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
-const token = localStorage.getItem('jwt');
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("jwt");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-
-api.defaults.headers.post['Content-Type'] = 'application/json';
+api.defaults.headers.post["Content-Type"] = "application/json";
 
 export default api;
